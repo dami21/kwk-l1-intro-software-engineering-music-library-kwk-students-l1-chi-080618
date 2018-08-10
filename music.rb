836b1d69d8fc8c947dd0b940c9d45a03bfbe80a7
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-10 14:08:41 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -13,6 +13,8 @@ music = {
 
 def list_artists
   puts music["Migos"]
+end 
+puts list_artists
   
 
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
